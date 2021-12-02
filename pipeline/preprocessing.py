0c1c0adf2e38dff862ba1495d2baf815ef0f8142
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2021-12-03 01:01:19 +0530

    Added an untested imlementation of TextVectorization API

--- pipeline/preprocessing.py
+++ pipeline/preprocessing.py
@@ -2,6 +2,7 @@ import os
 import pickle
 import tensorflow as tf
 from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.layers import TextVectorization
 
 from features import Feature
 
@@ -23,6 +24,14 @@ def _fill_in_missing(x):
             tf.SparseTensor(x.indices, x.values, [x.dense_shape[0], 1]),
             default_value), axis=1)
 
+MAX_SEQUENCE_LENGTH = 250
+VOCAB_SIZE = 20000
+
+int_vectorize_layer = TextVectorization(
+    max_tokens=VOCAB_SIZE,
+    output_mode='int',
+    output_sequence_length=MAX_SEQUENCE_LENGTH)
+
 
 def preprocessing_fn(inputs, custom_config):
     """Preprocesses Dataset."""
@@ -32,16 +41,23 @@ def preprocessing_fn(inputs, custom_config):
     os.system(f'gsutil -m cp {tokenizer_path} ./')
     
     with open(os.path.basename(tokenizer_path), 'rb') as handle:
-        tokenizer = pickle.load(handle)
-    
+        vectorizer_layer = pickle.load(handle)
+
     outputs = {}
-
+    
     key = Feature.transformed_name(Feature.FEATURE_KEY)
-    # Fill in missing values and create dense tensors.
-    outputs[key] = _fill_in_missing(inputs[Feature.FEATURE_KEY])
-
-    outputs[key] = tokenizer.texts_to_sequences(outputs[key])
-    outputs[key] = sequence.pad_sequences(outputs[key], maxlen=MAX_SEQUENCE_LENGTH)
+    # Fill in missing values and create dense tensors.    
+    
+    
+    # int_vectorize_layer.adapt(inputs[Feature.FEATURE_KEY])
+    
+    text = tf.expand_dims(inputs[Feature.FEATURE_KEY], -1)
+    outputs[key] = int_vectorize_layer(text)
+    
+    
+    # outputs[key] = _fill_in_missing(inputs[Feature.FEATURE_KEY])
+    # outputs[key] = tokenizer.texts_to_sequences(outputs[key])
+    # outputs[key] = sequence.pad_sequences(outputs[key], maxlen=MAX_SEQUENCE_LENGTH)
 
     # Convert label to dense tensor.
     outputs[Feature.transformed_name(Feature.LABEL_KEY)] = _fill_in_missing(inputs[Feature.LABEL_KEY])
