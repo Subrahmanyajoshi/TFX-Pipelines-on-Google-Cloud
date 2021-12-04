96c14b0e21f7f69d4d74bbb6fab9f172a41aa190
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2021-12-04 16:20:36 +0530

    Switching to forest cover type dataset

--- pipeline/features.py
+++ pipeline/features.py
@@ -1,8 +1,14 @@
 
 class Feature(object):
-    FEATURE_KEY = 'input'
-    LABEL_KEY = 'labels'
-    NUM_CLASSES = 2
+    NUMERIC_FEATURE_KEYS = [
+        'Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology',
+        'Vertical_Distance_To_Hydrology', 'Horizontal_Distance_To_Roadways',
+        'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm',
+        'Horizontal_Distance_To_Fire_Points'
+    ]
+    CATEGORICAL_FEATURE_KEYS = ['Wilderness_Area', 'Soil_Type']
+    LABEL_KEY = 'Cover_Type'
+    NUM_CLASSES = 7
 
     @staticmethod
     def transformed_name(key):
--- pipeline/preprocessing.py
+++ pipeline/preprocessing.py
@@ -6,8 +6,6 @@ from tensorflow.keras.layers import TextVectorization
 
 from features import Feature
 
-FEATURE_KEY = 'input'
-MAX_SEQUENCE_LENGTH = 500
 
 def _fill_in_missing(x):
     """Replace missing values in a SparseTensor.
@@ -24,42 +22,24 @@ def _fill_in_missing(x):
             tf.SparseTensor(x.indices, x.values, [x.dense_shape[0], 1]),
             default_value), axis=1)
 
-MAX_SEQUENCE_LENGTH = 250
-VOCAB_SIZE = 20000
 
-int_vectorize_layer = TextVectorization(
-    max_tokens=VOCAB_SIZE,
-    output_mode='int',
-    output_sequence_length=MAX_SEQUENCE_LENGTH)
-
-
-def preprocessing_fn(inputs, custom_config):
+def preprocessing_fn(inputs):
     """Preprocesses Dataset."""
     
-    tokenizer_path = custom_config.get('tokenizer_path')
-    
-    os.system(f'gsutil -m cp {tokenizer_path} ./')
-    
-    with open(os.path.basename(tokenizer_path), 'rb') as handle:
-        vectorizer_layer = pickle.load(handle)
-
     outputs = {}
-    
-    key = Feature.transformed_name(Feature.FEATURE_KEY)
-    # Fill in missing values and create dense tensors.    
-    
-    
-    # int_vectorize_layer.adapt(inputs[Feature.FEATURE_KEY])
-    
-    text = tf.expand_dims(inputs[Feature.FEATURE_KEY], -1)
-    outputs[key] = int_vectorize_layer(text)
-    
-    
-    # outputs[key] = _fill_in_missing(inputs[Feature.FEATURE_KEY])
-    # outputs[key] = tokenizer.texts_to_sequences(outputs[key])
-    # outputs[key] = sequence.pad_sequences(outputs[key], maxlen=MAX_SEQUENCE_LENGTH)
 
-    # Convert label to dense tensor.
-    outputs[Feature.transformed_name(Feature.LABEL_KEY)] = _fill_in_missing(inputs[Feature.LABEL_KEY])
+    # Scale numerical features.
+    for key in Feature.NUMERIC_FEATURE_KEYS:
+        outputs[Feature.transformed_name(key)] = tft.scale_to_z_score(
+            _fill_in_missing(inputs[key]))
+
+    # Generate vocabularies and maps categorical features.
+    for key in Feature.CATEGORICAL_FEATURE_KEYS:
+        outputs[Feature.transformed_name(key)] = tft.compute_and_apply_vocabulary(
+            x=_fill_in_missing(inputs[key]), num_oov_buckets=1, vocab_filename=key)
+
+    # Convert Cover_Type to dense tensor.
+    outputs[Feature.transformed_name(Feature.LABEL_KEY)] = _fill_in_missing(
+        inputs[Feature.LABEL_KEY])
 
     return outputs
